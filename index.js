790c14284f3fba1f018646a95184040d272649f4
Duckie <aryanbaburajan2007@gmail.com>
2022-08-13 20:46:53 +0530

    e

---
+++ index.js
@@ -0,0 +1,54 @@
+let menu = document.getElementById("menu");
+let selectMenu = document.getElementById("select-menu");
+menu.style.display = "none";
+selectMenu.style.display = "none";
+
+document.addEventListener(
+  "contextmenu",
+  function (e) {
+    e.preventDefault();
+    menu.style.display = "block";
+    menu.style.left = e.pageX + "px";
+    menu.style.top = e.pageY + "px";
+  },
+  false
+);
+
+function getSelectedText() {
+  var text = "";
+  if (typeof window.getSelection != "undefined") {
+    text = window.getSelection().toString();
+  } else if (
+    typeof document.selection != "undefined" &&
+    document.selection.type == "Text"
+  ) {
+    text = document.selection.createRange().text;
+  }
+  return text;
+}
+
+function textSelected(e) {
+  var selectedText = getSelectedText();
+  if (selectedText) {
+    selectMenu.style.display = "block";
+    selectMenu.style.left = e.pageX + "px";
+    selectMenu.style.top = e.pageY + "px";
+  } else {
+    selectMenu.style.display = "none";
+  }
+}
+
+document.onmouseup = function (e) {
+  textSelected(e);
+  menu.style.display = "none";
+};
+
+document.onmousedown = function (e) {};
+
+function copyLinkToHighlight() {
+  navigator.clipboard.writeText(
+    location.href +
+      "#:~:text=" +
+      encodeURIComponent(window.getSelection().getRangeAt(0).toString())
+  );
+}
